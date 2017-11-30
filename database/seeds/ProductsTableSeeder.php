9d5c7239b9a33dde4c95f72ccb59f607cd3909fe
viktor <sydorenkovd@gmail.com>
2017-11-30 17:55:12 +0200

    init

---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = \Faker\Factory::create();
+
+        // Create 50 product records
+        for ($i = 0; $i < 50; $i++) {
+            \App\Product::create([
+                'title' => $faker->title,
+                'description' => $faker->paragraph,
+                'price' => $faker->randomNumber(2),
+                'availability' => $faker->boolean(50)
+            ]);
+        }
+    }
+}
